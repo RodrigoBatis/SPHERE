1dc57d429fe34b6482f8fd25cab04a01f8271979
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-06 21:59:55 -0300

    Adicionado form de cadastro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ModeToggle } from "@/components/ui/mode-toggle";
-import { Tabs, TabsTrigger, TabsList, TabsContent } from '@/components/ui/tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Label } from "@radix-ui/react-label";
 import Image from "next/image";
 
@@ -10,16 +10,16 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-between">
 
       <nav className="absolute right-2 top-2">
-        <ModeToggle/>
+        <ModeToggle />
       </nav>
 
       <aside className="hidden lg:block">
-        <Image src="/login.png" alt="" width={554} height={832}/>
+        <Image src="/login.png" alt="" width={554} height={832} />
       </aside>
 
       <section className="flex flex-col items-center w-full space-y-7">
         <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48"/>
+          <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48" />
         </svg>
 
         <h1 className="text-4xl font-extrabold uppercase">Sphere</h1>
@@ -37,18 +37,33 @@ export default function Home() {
               <Input id="email" />
               <Label htmlFor="senha">senha</Label>
               <Input id="senha" type="password" />
+              <div className="flex flex-col items-center w-full space-y-7">
+                <Button>Entrar</Button>
+              </div>
             </form>
           </TabsContent>
 
           <TabsContent value="registrar">
-            <p>criar conta </p>
+            <form action="" className="space-y-2">
+              <Label htmlFor="nome">nome</Label>
+              <Input id="nome" />
+              <Label htmlFor="bio">bio</Label>
+              <Input id="bio" />
+              <Label htmlFor="email">email</Label>
+              <Input id="email" />
+              <Label htmlFor="senha">senha</Label>
+              <Input id="senha" type="password" />
+              <div className="flex flex-col items-center w-full space-y-7">
+                <Button>criar conta</Button>
+              </div>
+            </form>
           </TabsContent>
 
         </Tabs>
 
-        <Button>Entrar</Button>
+
       </section>
-      
+
     </main>
   );
 }
